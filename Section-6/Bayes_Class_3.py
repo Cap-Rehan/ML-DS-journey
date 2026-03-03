95d9663c406622679b0e1773d094a17b7520e40a
Cap-Rehan <yasiransari370@gmail.com>
2026-03-03 16:49:51 +0530

    training complete

--- Section-6/Bayes_Class_3.py
+++ Section-6/Bayes_Class_3.py
@@ -25,6 +25,16 @@ TRAINING_DATA_FILE = "SpamData/02_Training/train-data.txt"
 TEST_DATA_FILE = "SpamData/02_Training/test-data.txt"
 VOCAB_SIZE = 3000
 
+FULL_TRAIN_MATRIX = "SpamData/02_Training/train-matrix.csv"
+FULL_TEST_MATRIX = "SpamData/02_Training/test-matrix.csv"
+
+PROB_TOKEN_SPAM = "SpamData/03_Testing/prob-spam.txt"
+PROB_TOKEN_HAM = "SpamData/03_Testing/prob-ham.txt"
+PROB_TOKEN_ALL = "SpamData/03_Testing/prob-tokens.txt"
+
+TEST_FEATURES= "SpamData/03_Testing/X-test.txt"
+TEST_TARGET= "SpamData/03_Testing/y-test.txt"
+
 # %%
 # Load Sparse Data from TXT
 
@@ -103,3 +113,122 @@ full_train_df = make_full_matrix(sparse_train, VOCAB_SIZE)
 full_test_df = make_full_matrix(sparse_test, VOCAB_SIZE)
 
 # %%
+full_train_df.to_csv(FULL_TRAIN_MATRIX)
+full_test_df.to_csv(FULL_TEST_MATRIX)
+
+# %% [markdown]
+# ## Training the Naive Bayes Model
+
+# %% [markdown]
+# ### Calculating Probabilities
+
+# %%
+print(full_train_df['CATEGORY'].value_counts())
+print(full_train_df.CATEGORY.size)
+print(len(full_train_df))
+
+# %%
+# probability of spam
+prob_spam = full_train_df['CATEGORY'].value_counts()[1]/len(full_train_df)
+prob_spam
+
+# %%
+# number of words / tokens
+full_train_features = full_train_df.loc[:, 0:]
+full_train_features.head()
+
+# %%
+words_per_email = full_train_features.sum(axis= 1)
+words_per_email.tail()
+
+# %%
+total_wc = words_per_email.sum()
+print(total_wc)
+
+# %%
+# number of tokens in spam and ham
+word_per_spam = (full_train_df.loc[full_train_df.CATEGORY == 1, 0:]).sum(axis= 1)
+word_per_ham = (full_train_df.loc[full_train_df.CATEGORY == 0, 0:]).sum(axis= 1)
+
+# %%
+spam_wc = word_per_spam.sum()
+print(spam_wc)
+ham_wc = word_per_ham.sum()
+print(ham_wc)
+
+# %%
+print(total_wc - spam_wc - ham_wc)
+
+# %%
+# which emails tend to be longer
+print(word_per_spam.mean())
+print(word_per_ham.mean())
+
+# %%
+# Summing all the tokens occuring in spam
+full_train_df
+
+# %%
+spam_train_tokens = full_train_features.loc[full_train_df.CATEGORY == 1]
+spam_train_tokens.tail()
+
+# %%
+spam_train_tokens.shape
+
+# %%
+summed_spam_tokens= spam_train_tokens.sum(axis= 0) + 1 # laplace smoothing
+summed_spam_tokens
+
+# %%
+ham_train_tokens= full_train_features.loc[full_train_df.CATEGORY == 0]
+summed_ham_tokens= ham_train_tokens.sum(axis= 0) + 1
+summed_ham_tokens
+
+# %%
+(summed_spam_tokens == 0).any()
+
+# %% [markdown]
+# ### $ P \left( Token \mid Spam \right) $
+
+# %%
+prob_tokens_spam = summed_spam_tokens / (spam_wc + VOCAB_SIZE)
+prob_tokens_spam
+
+# %%
+print(prob_tokens_spam.sum())
+
+# %% [markdown]
+# ### $ P \left( Token \mid Ham \right) $
+
+# %%
+prob_tokens_ham = summed_ham_tokens / (ham_wc + VOCAB_SIZE)
+print(prob_tokens_ham.sum())
+
+# %% [markdown]
+# ### $ P\left( Token \right) $
+
+# %%
+prob_tokens_all = full_train_features.sum(axis= 0) / total_wc
+print(prob_tokens_all.sum())
+
+# %% [markdown]
+# ### Saving the trained model
+
+# %%
+np.savetxt(PROB_TOKEN_SPAM, prob_tokens_spam)
+np.savetxt(PROB_TOKEN_HAM, prob_tokens_ham)
+np.savetxt(PROB_TOKEN_ALL, prob_tokens_all)
+
+# %% [markdown]
+# ### Preparing Testing Files
+
+# %%
+X_test= full_test_df.loc[:, full_test_df.columns != 'CATEGORY']
+y_test= full_test_df.CATEGORY
+
+# %%
+y_test
+
+# %%
+np.savetxt(TEST_FEATURES, X_test)
+np.savetxt(TEST_TARGET, y_test)
